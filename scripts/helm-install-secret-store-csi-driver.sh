d5e124350b8629ee45f4a9e4f520b59a67ea6b8f
Leif Eriksen <leif.eriksen.au@gmail.com>
2026-03-09 20:01:52 +1100

    add secret-store csi driver, minor tweaks in config maps

--- scripts/helm-install-secret-store-csi-driver.sh
+++ scripts/helm-install-secret-store-csi-driver.sh
@@ -3,3 +3,4 @@
 set -euxo pipefail
 
 helm repo add secrets-store-csi-driver https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts
+helm install csi-secrets-store secrets-store-csi-driver/secrets-store-csi-driver --namespace kube-system
